7a3292d32af5be38a0e018ece7ca736cf3887455
Fernando Destefani <henriqueorei@gmail.com>
2025-10-26 16:51:32 -0300

    break form into smaller reusable components

--- src/components/BirthPlanForm.jsx
+++ src/components/BirthPlanForm.jsx
@@ -5,67 +5,81 @@ export default function BirthPlanForm() {
         Fields marked with <span>*</span> are required.
       </em>
 
-      <InputField label="Mother's Name" id="motherName" name="motherName" type="text" placeholder="Enter your full name" required={true} title="Enter your full name"/>
+      <InputField
+        label="Mother's Name"
+        id="motherName"
+        name="motherName"
+        type="text"
+        placeholder="Enter your full name"
+        required={true}
+        title="Enter your full name"
+      />
 
-      <InputField label="Expected Due Date" id="dueDate" name="dueDate" type="date" placeholder="Select your due date" title="Select your due date"/>
+      <InputField
+        label="Expected Due Date"
+        id="dueDate"
+        name="dueDate"
+        type="date"
+        placeholder="Select your due date"
+        title="Select your due date"
+      />
 
-      <InputField label="Support Person's Name" id="companion" name="companion" type="text" placeholder="Enter companion's name" title="Enter the name of your support person"/>
+      <InputField
+        label="Support Person's Name"
+        id="companion"
+        name="companion"
+        type="text"
+        placeholder="Enter companion's name"
+        title="Enter the name of your support person"
+      />
 
-      <fieldset>
-        <legend>
-          Place of Birth Preference<span>*</span>:
-        </legend>
-        <label>
-          <input type="radio" name="birthPlace" value="hospital" required />
-          Hospital
-        </label>
+      <RadioGroup
+        legend="Place of Birth Preference"
+        name="birthPlace"
+        required={true}
+        options={[
+          { value: "hospital", label: "Hospital", isFirst: true },
+          { value: "birthCenter", label: "Birth Center" },
+          { value: "home", label: "Home" },
+        ]}
+      />
 
-        <label>
-          <input type="radio" name="birthPlace" value="birthCenter" />
-          Birth Center
-        </label>
+      <CheckboxGroup
+        legend="Environment Preferences"
+        name="environment"
+        options={[
+          { value: "dimLights", label: "I prefer dim lights" },
+          { value: "quietRoom", label: "I prefer a quiet room" },
+          { value: "music", label: "I would like to listen to music" },
+        ]}
+      />
 
-        <label>
-          <input type="radio" name="birthPlace" value="home" />
-          Home
-        </label>
-      </fieldset>
+      <RadioGroup
+        legend="Mobility During Labor"
+        name="mobility"
+        required={true}
+        options={[
+          {
+            value: "freeMovement",
+            label: "I would like to have the freedom to walk and move",
+            isFirst: true,
+          },
+          { value: "mostlyBed", label: "I prefer to stay mostly in bed" },
+          {
+            value: "decideDuringLabor",
+            label: "I prefer to decide during labor",
+          },
+        ]}
+      />
 
-      <fieldset>
-        <legend>Environment Preferences</legend>
-        <label>
-          <input type="checkbox" name="environment" value="dimLights" />I prefer
-          dim lights
-        </label>
-
-        <label>
-          <input type="checkbox" name="environment" value="quietRoom" />I prefer
-          a quiet room
-        </label>
-
-        <label>
-          <input type="checkbox" name="environment" value="music" />I would like
-          to listen to music
-        </label>
-      </fieldset>
-
-      <fieldset>
-        <legend>Mobility During Labor</legend>
-        <label>
-          <input type="radio" name="mobility" value="freeMovement" required />I
-          would like to have the freedom to walk and move
-        </label>
-
-        <label>
-          <input type="radio" name="mobility" value="mostlyBed" />I prefer to
-          stay mostly in bed
-        </label>
-
-        <label>
-          <input type="radio" name="mobility" value="decideDuringLabor" />I
-          prefer to decide during labor
-        </label>
-      </fieldset>
+      <CheckboxGroup legend="Relief Options (Non-Pharmacological)" name="painRelief" options={[{value: "bath", label: "Bath"}, 
+        {value: "massage", label: "Bath"},
+        {value: "birthingBall", label: "Birthing ball"},
+        {value: "squats", label: "Squats"},
+        {value: "walking", label: "Walking/ Deambulation"},
+        {value: "breathingTechniques", label: "Breathing Techniques"},
+        {value: "decideOnSpot", label: "Prefer to decide on the spot"}
+      ]}/>  
 
       <p>These preferences may change depending on medical needs</p>
     </form>
@@ -83,7 +97,8 @@ function InputField({
   return (
     <div>
       <label htmlFor={name}>
-        {label}{required && <span>*</span>}:
+        {label}
+        {required && <span>*</span>}:
       </label>
       <input
         type={type}
@@ -94,5 +109,41 @@ function InputField({
         title={title}
       />
     </div>
+  );
+}
+
+function RadioGroup({ legend, name, options, required = false, value }) {
+  return (
+    <fieldset>
+      <legend>
+        {legend}
+        {required && <span>*</span>}:
+      </legend>
+
+      {options.map((option) => (
+        <label key={option.value}>
+          <input
+            type="radio"
+            name={name}
+            required={required && option.isFirst}
+          />
+          {option.label}
+        </label>
+      ))}
+    </fieldset>
+  );
+}
+
+function CheckboxGroup({ legend, options }) {
+  return (
+    <fieldset>
+      <legend>{legend}</legend>
+      {options.map((option) => (
+        <label key={option.value}>
+          <input type="checkbox" name={name} />
+          {option.label}
+        </label>
+      ))}
+    </fieldset>
   );
 }
